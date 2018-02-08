bc7dc995d1000facbafedf6b4e0ee75c1bc7e33d
erikgoe <erikgoe@users.noreply.github.com>
2018-02-08 14:12:31 +0100

    switched to java 1.7

--- src/LocalLogic.java
+++ src/LocalLogic.java
@@ -13,10 +13,10 @@ class LocalLogic {
 	void update_new_map(Field[][] fields) {
 		state.field_resources = new HashMap<>();
 		for (Resource res : Resource.values())
-			state.field_resources.put(res, new ArrayList<>());
+			state.field_resources.put(res, new ArrayList<Vector2f>());
 		state.field_numbers = new HashMap<>();
 		for (byte i = 2; i < Map.number_count + 2; i++)
-			state.field_numbers.put(i, new ArrayList<>());
+			state.field_numbers.put(i, new ArrayList<Vector2f>());
 
 		for (int x = 0; x < fields.length; x++) {
 			for (int y = 0; y < fields[x].length; y++) {
